8e7fc92c67bfc8414b8f997d6204eea1ac02adf9
souravghatak-007 <sourav.influxiq@gmail.com>
2020-02-22 11:56:16 +0530

    delete

--- src/app/http-loader.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-@Injectable({
-  providedIn: 'root'
-})
-export class HttpLoaderService {
-  public isLoading = new BehaviorSubject(false);
-  constructor() {}
-}
